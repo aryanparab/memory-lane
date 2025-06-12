3c1832513e7f3b097b3a63f0892651150fce5383
Aryan Parab <66548981+aryanparab@users.noreply.github.com>
2025-06-11 19:37:33 -0700

    smallll

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,19 +1,21 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
-    providers :[
-        GoogleProvider({
-            clientId:process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ],
-    callbacks:{
-        async session({session,token}){
-            session.user.id = token.sub;
-            return session
-        }
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    })
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      session.user.id = token.sub;
+      return session;
     }
-});
+  }
+};
 
-export {handler as GET, handler as POST};
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
